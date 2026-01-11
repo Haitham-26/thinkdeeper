6045dae8c3ddfed00700f020c8afd94f3a80604a
Haitham-26 <haitham.waki.work@gmail.com>
2026-01-11 16:57:35 +0300

    possible fix for the token set

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,9 +1,23 @@
 import { proxyRequest } from "@/tools/proxyRequest";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  return await proxyRequest("/auth/login", {
+  const expressRes = await proxyRequest("/auth/login", {
     method: "POST",
     data: await req.json(),
   });
+
+  const { token } = await expressRes.json();
+
+  const response = NextResponse.json({ success: true });
+
+  response.cookies.set("token", token, {
+    httpOnly: true,
+    secure: process.env.NEXT_PUBLIC_NODE_ENV === "production",
+    sameSite: "none",
+    path: "/",
+    maxAge: 60 * 60 * 24,
+  });
+
+  return response;
 }
