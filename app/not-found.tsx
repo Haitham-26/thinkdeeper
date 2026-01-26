7af0463c7476c29c69bc656a1691dfb9ac9674c9
Haitham-26 <haitham.waki.work@gmail.com>
2026-01-26 16:11:08 +0300

    updated not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+import { Icon } from "./components/Icon";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+
 export default function NotFound() {
   return (
-    <main className="flex flex-col gap-4 items-center justify-center">
-      <h1 className="text-white font-bold text-2xl">الصفحة غير موجودة</h1>
-      <p className="text-gray-200">الصفحة المطلوبة غير موجودة أو تم حذفها</p>
-    </main>
+    <div className="flex flex-col gap-4 items-center justify-center w-full">
+      <h1 className="text-accent font-bold text-2xl">الصفحة غير موجودة</h1>
+      <p className="text-accent">الصفحة المطلوبة غير موجودة أو تم حذفها</p>
+
+      <Link
+        href="/auth/signup"
+        className="py-4 px-12 rounded-2xl font-bold text-lg bg-accent text-white shadow-xl shadow-accent/30 hover:-translate-y-1 transition-all duration-300"
+      >
+        العودة للرئيسية
+        <Icon icon={faHome} className="mr-3" />
+      </Link>
+    </div>
   );
 }
